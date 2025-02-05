cd8f110ba6f309f99392521435def4181ba23555
Gerardo Mundo <ger.mundo66@gmail.com>
2025-02-04 19:35:43 -0600

    Fix model properties

--- api/models.py
+++ api/models.py
@@ -1,12 +1,11 @@
-from typing import Required
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(Required, max_length=200)
-    last_name = models.CharField(Required, max_length=200)
-    email = models.EmailField(Required, max_length=60)
-    last_session = models.DateField(null=True)
-    created_at = models.DateField(auto_created=True, editable=False)
+    name = models.CharField(blank=False, null=False, max_length=200)
+    last_name = models.CharField(blank=False, null=False, max_length=200)
+    email = models.EmailField(blank=False, null=False, unique=True, max_length=60)
+    last_session = models.DateField(blank=True, null=True, default=None)
+    created_at = models.DateField(auto_now_add=True, editable=False)
     # One example on how to define a relationship between models
     # question = models.ForeignKey(Question, on_delete=models.CASCADE)
 
