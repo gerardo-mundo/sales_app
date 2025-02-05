88c70e0dea44e3738e7bf4c47180d50a38609d55
Gerardo Mundo <ger.mundo66@gmail.com>
2025-02-04 19:27:30 -0600

    Modify User model

--- api/models.py
+++ api/models.py
@@ -1,10 +1,11 @@
+from typing import Required
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
-    email = models.CharField(max_length=80)
-    last_session = models.DateField()
+    name = models.CharField(Required, max_length=200)
+    last_name = models.CharField(Required, max_length=200)
+    email = models.EmailField(Required, max_length=60)
+    last_session = models.DateField(null=True)
     created_at = models.DateField(auto_created=True, editable=False)
     # One example on how to define a relationship between models
     # question = models.ForeignKey(Question, on_delete=models.CASCADE)
